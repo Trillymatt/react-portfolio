be45a080074c4eacd97d27690ac7cf19238585ae
Matthew Norman <normanmatthew38@gmail.com>
2024-08-04 12:58:17 -0700

    adding files for Personal Website as well as code for differnt pages

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { intializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestoe/lite';
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+const app = intializeApp(firebaseConfig);
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
